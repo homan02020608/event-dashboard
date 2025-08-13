61bcc6fd52dcd2c49f7418814876aa7d6bf01dcb
homan02020608 <homan090402@gmail.com>
2025-08-13 17:40:12 +0900

    setup routing

---
+++ src/app/expenses/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>費用管理一覧</div>
+  )
+}
+
+export default page
---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>参戦写真一覧</div>
+  )
+}
+
+export default page
