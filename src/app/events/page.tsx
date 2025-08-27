fb84198eb1c0a79eaa21ef12bf2cb3cd9e267784
homan02020608 <homan090402@gmail.com>
2025-08-27 23:18:54 +0900

    fix

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,3 +1,4 @@
+import AddEventButton from '@/components/eventUi/AddEventButton'
 import EventCard from '@/components/eventUi/EventCard'
 import { prisma } from '@/lib/prisma'
 import { EventCardTypesSource } from '@/types/type'
@@ -5,15 +6,11 @@ import React from 'react'
 
 const page = async () => {
   const events = await prisma.event.findMany()
-  const user = await prisma.user.findMany({
-    include: {
-      event: true
-    }
-  })
-  
+
   return (
     <div className="bg-white/70 shadow-2xl rounded-lg m-2 mx-4 p-2">
       <h1 className='text-2xl p-2 flex-Center'>参戦履歴</h1>
+      <AddEventButton />
       <div>
         {events.map((event: EventCardTypesSource) => (
           <div key={event.id} className='m-4 my-8 bg-white rounded-xl text-black *:px-4 shadow-xl'>
