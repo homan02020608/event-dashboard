f0a7ccb8d09566ad8a3060380dba9b232b25dfa0
homan02020608 <homan090402@gmail.com>
2025-08-30 21:02:32 +0900

    add : EventCalendar , DataPicker, TimePicker

--- src/components/eventUi/AddEventButton.tsx
+++ src/components/eventUi/AddEventButton.tsx
@@ -23,13 +23,27 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Calendar } from "@/components/ui/calendar"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { CalendarIcon } from 'lucide-react'
+import { format } from "date-fns"
+import { cn } from '@/lib/utils'
+import { createEvent } from '@/app/action/action'
+
 
 const formSchema = z.object({
     eventTitle: z.string().min(1),
-    date: z.string(),
     region: z.string(),
     venue: z.string().min(1),
     seat: z.string().min(1),
+    date: z.date({
+        message: "A date of birth is required"
+    }),
+    eventStartTime: z.string().min(1),
 })
 
 const AddEventButton = () => {
@@ -37,12 +51,26 @@ const AddEventButton = () => {
         resolver: zodResolver(formSchema),
         defaultValues: {
             eventTitle: '',
-            date: '',
             region: '',
             venue: '',
             seat: '',
+            date: new Date(),
+            eventStartTime: '12:00:00',
         },
     })
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        const formData = new FormData();
+
+        formData.append('eventTitle', values.eventTitle)
+        formData.append('region', values.region)
+        formData.append('venue', values.venue)
+        formData.append('seat', values.seat)
+        formData.append('date', values.date.toISOString())
+        formData.append('eventStartTime', values.eventStartTime)
+        console.log(values)
+        await createEvent(formData) 
+    }
     return (
         <Sheet>
             <SheetTrigger asChild className='mx-4'>
@@ -53,7 +81,7 @@ const AddEventButton = () => {
                     <SheetTitle>参戦イベント追加</SheetTitle>
                 </SheetHeader>
                 <Form {...form}>
-                    <form className="space-y-8 p-2 m-4">
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 p-2 m-4">
                         <FormField
                             control={form.control}
                             name="eventTitle"
@@ -106,15 +134,69 @@ const AddEventButton = () => {
                                 </FormItem>
                             )}
                         />
-                        <Button type="submit">Submit</Button>
+                        <FormField
+                            control={form.control}
+                            name="date"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>公演日</FormLabel>
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <FormControl>
+                                                <Button
+                                                    variant={"outline"}
+                                                    className={cn(
+                                                        "w-[240px] pl-3 text-left font-normal",
+                                                        !field.value && "text-muted-foreground"
+                                                    )}
+                                                >
+                                                    {field.value ? (
+                                                        format(field.value, "PPP")
+                                                    ) : (
+                                                        <span>Pick a date</span>
+                                                    )}
+                                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                </Button>
+                                            </FormControl>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0" align="start">
+                                            <Calendar
+                                                mode="single"
+                                                selected={field.value}
+                                                onSelect={field.onChange}
+                                                disabled={(date) =>
+                                                    date < new Date("1900-01-01")
+                                                }
+                                                captionLayout="dropdown"
+                                            />
+                                        </PopoverContent>
+                                    </Popover>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="eventStartTime"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>開演時刻</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type='time'
+                                            id='time-picker'
+                                            className="bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none" 
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <SheetClose asChild className='border w-full'>
+                            <Button type="submit" className='hover:underline ' variant={'outline'}>追加</Button>
+                        </SheetClose>
                     </form>
                 </Form>
-                <SheetFooter>
-                    <Button type="submit">Save changes</Button>
-                    <SheetClose asChild>
-                        <Button variant="outline">Close</Button>
-                    </SheetClose>
-                </SheetFooter>
             </SheetContent>
         </Sheet>
     )
--- src/components/eventUi/EventCard.tsx
+++ src/components/eventUi/EventCard.tsx
@@ -7,14 +7,14 @@ import {
 } from "@/components/ui/accordion"
 import { EventCardTypesSource } from '@/types/type'
 
-const EventCard = ({ title, date, venue, seat }: EventCardTypesSource) => {
+const EventCard = ({ title, date, venue, seat, eventStartTime }: EventCardTypesSource) => {
 
     return (
         <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
                 <AccordionTrigger >{title}</AccordionTrigger>
                 <AccordionContent className='flex-Start gap-10'>
-                    <div>公演日:<span>{date?.toLocaleDateString('ja-JP')}</span></div>
+                    <div>公演日:<span>{date?.toLocaleDateString('ja-JP')}</span><span className='ml-2'>{eventStartTime}</span></div>
                     <div>会場:<span>{venue}</span></div>
                     <div>座席:<span>{seat}</span></div>
                 </AccordionContent>
