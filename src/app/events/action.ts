3d5d2bca2182c283dc6b9437ffe06c3df65e4efe
homan02020608 <homan090402@gmail.com>
2026-02-14 19:06:18 +0900

    feat : handle success and error states in createEvent

--- src/app/events/action.ts
+++ src/app/events/action.ts
@@ -5,25 +5,31 @@ import { revalidatePath } from "next/cache"
 
 // ログインしているauthorIdでEvent項目を追加
 export async function createEvent(formData: FormData) {
-    const supabase = createClient()
-    const { data: { user } } = await (await supabase).auth.getUser()
+    try {
+        const supabase = createClient()
+        const { data: { user } } = await (await supabase).auth.getUser()
 
-    if (!user) {
-        throw new Error('ログインしてください。')
-    }
-
-    await prisma.event.create({
-        data: {
-            authorId: user?.id || "",
-            title: formData.get('eventTitle') as string,
-            date: new Date(formData.get('date') as string),
-            eventStartTime: formData.get('eventStartTime') as string,
-            region: formData.get('region') as string,
-            venue: formData.get('venue') as string,
-            seat: formData.get('seat') as string,
+        if (!user) {
+            throw new Error('ログインしてください。')
         }
-    })
-    revalidatePath('/events')
+
+        await prisma.event.create({
+            data: {
+                authorId: user?.id || "",
+                title: formData.get('eventTitle') as string,
+                date: new Date(formData.get('date') as string),
+                eventStartTime: formData.get('eventStartTime') as string,
+                region: formData.get('region') as string,
+                venue: formData.get('venue') as string,
+                seat: formData.get('seat') as string,
+            }
+        })
+        revalidatePath('/events')
+        return { success: true }
+    } catch (error) {
+        console.error('db保存エラー:', error)
+        return { success: false, message: '送信失敗' }
+    }
 }
 
 export async function deleteEvents(eventIds: string[]) {
